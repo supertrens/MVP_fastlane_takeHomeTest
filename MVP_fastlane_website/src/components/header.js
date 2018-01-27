e9ddcd11b7dc2e09962bdf18e3f87ab636a55c8b
Ernst Pierre <pitrens.studio@gmail.com>
2018-01-27 22:00:16 +0800

    done implementing the BEM architecture. Now I am gonna get my hand dirty with styling

--- MVP_fastlane_website/src/components/header.js
+++ MVP_fastlane_website/src/components/header.js
@@ -2,16 +2,21 @@ import React  from'react'
 import Link   from 'gatsby-link'
 import logo from "../img/logo.png";
 
+
 const Header = () => (
   <header className="header">
-    <div className="logo-box">
-      <img src={logo} alt="MVP Fastlane Logo" className="logo" />
+    <div className="header__logo-box">
+      <img src={logo} alt="MVP Fastlane Logo" className="header__logo" />
     </div>
-    <div className="text-box">
+    <div className="header__text-box">
       <h1 className="heading-primary">
-        <span className="heading-primary-main">MVP Fastlane</span>
-        <span className="heading-primary-sub">Level Up</span>
+        <span className="heading-primary--main">MVP Fastlane</span>
+        <span className="heading-primary--sub">Level Up</span>
       </h1>
+
+      <Link to="/page-2/" className="btn btn--white btn--animated">
+        Our Team
+      </Link>
     </div>
   </header>
 );
--- MVP_fastlane_website/src/pages/page-2.js
+++ MVP_fastlane_website/src/pages/page-2.js
@@ -2,11 +2,59 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const SecondPage = () => (
-  <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
-)
+  <section className="grid-test">
+    <div className="row">
+      <div className="col-1-of-2"> ol-1-of-2</div>
+      <div className="col-1-of-2"> ol-1-of-2</div>
+    </div>
+
+    <div className="row">
+      <div className="col-1-of-3"> ol-1-of-3</div>
+      <div className="col-1-of-3"> ol-1-of-3</div>
+      <div className="col-1-of-3"> ol-1-of-3</div>
+    </div>
+
+    <div className="row">
+      <div className="col-1-of-3"> ol-1-of-3</div>
+      <div className="col-2-of-3"> ol-3-of-3</div>
+    </div>
+  </section>
+  // <section className="grid-test">
+  //   <h1>MVP Fastlane</h1>
+  //       <h2>Our Team</h2>
+  //       <div>
+  //         <section>
+  //           <h3>Leadership</h3>
+  //           <div>
+  //             <h4>Tiwen Lin</h4>
+  //             Founder &amp; CEO
+  //           </div>
+  //           <div>
+  //             <h4>Chiayu Wu</h4>
+  //             Co-founder
+  //           </div>
+  //         </section>
+  //         <section>
+  //           <h3>Engineering</h3>
+  //           <div>
+  //             <h4>Philip Chen</h4>
+  //             Software Engineer, Full-Stack
+  //           </div>
+  //           <div>
+  //             <h4>Peihsuan Chang</h4>
+  //             Software Engineer, Full-Stack
+  //           </div>
+  //         </section>
+  //         <section>
+  //           <h3>Product</h3>
+  //           <div>
+  //             <h4>Wanchen Huang</h4>
+  //             Product Manager
+  //           </div>
+  //         </section>
+  //       </div>
+  //   <Link to="/">Go back to the homepage</Link>
+  // </section>
+);
 
 export default SecondPage
